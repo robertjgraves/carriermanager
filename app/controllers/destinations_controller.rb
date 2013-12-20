423267a0913e2c7562ca03a81d2c3ac5cc5a43e5
Robert Graves <robertjgraves@gmail.com>
2013-12-20 13:23:46 -0800

    destinations controller

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,2 +1,45 @@
 class DestinationsController < ApplicationController
+	def new
+		@destination = Destination.new
+	end
+
+	def create
+		@destination = Destination.new(params[:destination].permit(:name))
+
+		if @destination.save
+			redirect_to destinations_path
+		else
+			render 'new'
+		end
+	end
+
+	def index
+		@destinations = Destination.all
+	end
+
+	def show
+		@destination = Destination.find(params[:id])
+	end
+
+	def edit
+		@destination = Destination.find(params[:id])
+	end
+
+	def update
+		@destination = Destination.find(params[:id])
+
+		if @destination.update(params[:destination].permit(:name))
+			redirect_to @destination
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@destination = Destination.find(params[:id])
+		@destination.destroy
+
+		redirect_to destinations_path
+	end
+
 end
